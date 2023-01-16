11c62d436ab94eccb3ebf0d90d74a80f83cafaee
thisiszy <zhiyhuang@student.eth.ch>
2023-01-16 22:44:00 +0100

    add helper files

---
+++ install.sh
@@ -0,0 +1,10 @@
+# install all extension modules
+pip install -r requirements.txt
+cd instant-nerf
+bash scripts/install_ext.sh
+
+# if you want to install manually, here is an example:
+cd raymarching
+python setup.py build_ext --inplace # build ext only, do not install (only can be used in the parent directory)
+pip install . # install to python path (you still need the raymarching/ folder, since this only install the built extension.)
+
